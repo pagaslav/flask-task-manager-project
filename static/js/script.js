12a4422efe03b260439b2ae5eb3206d77f3368f8
Artem Bryzh <aab@AMBM2.local>
2024-06-14 16:47:22 +0100

    Add collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-  });
+/*
+    jQuery for MaterializeCSS initialization
+*/
+
+$(document).ready(function () {
+  $(".sidenav").sidenav({edge: "right"});
+  $(".collapsible").collapsible();
+});
+
+
+
